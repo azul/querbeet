4806cd0358f0f9f8d43dd9bba2d8711f970b7210
Azul <azul@riseup.net>
2011-11-19 00:27:43 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreetest::Application.config.secret_token = 'a406aa89e7c44b22b62d4994e37b933c6a18ffcda1cee91157fe8e78baf353164c586e632b799f38f8319409ed5653810859d083bc85803bb03f53d6823ca3f4'
