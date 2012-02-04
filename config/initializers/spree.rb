82291a25a196cd72a7bd89d446a0480789440a1f
Azul <azul@riseup.net>
2012-02-04 18:43:14 +0100

    don't require state

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,2 +1,3 @@
 Spree::Config.set(:allow_ssl_in_production => false)
 Spree::Config.set(:logo => 'http://www.querbeet-jungpflanzen.de/wp-content/uploads/2011/10/querbeet_header.jpg')
+Spree::Config.set(:address_requires_state => false)
