232fd3f4c65ae950d8c90beac50c1d8ccd4a6ed4
Azul <azul@riseup.net>
2012-02-24 19:44:38 +0100

    updated css, uploaded images, use local header image

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,4 +1,4 @@
 Spree::Config.set(:allow_ssl_in_production => false)
-Spree::Config.set(:logo => 'http://www.querbeet-jungpflanzen.de/wp-content/uploads/2011/10/querbeet_header.jpg')
+Spree::Config.set(:logo => '/assets/store/querbeet_header.jpg')
 Spree::Config.set(:address_requires_state => false)
 Spree::Config.set(:products_per_page => 100)
