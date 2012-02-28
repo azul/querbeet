35f08ddb6a98877192f56eed74e6c6a3fd1ea963
Azul <azul@riseup.net>
2012-02-28 09:42:41 +0100

    no welcome message above the products

--- app/overrides/add_welcome_message_on_home.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "home/index",
-                     :name => "menu below the header",
-                     :insert_before => "div[data-hook=homepage_products]",
-                     :partial => 'shared/welcome')
