9524634e34a994408ee59602e04e97116a26d455
Azul <azul@riseup.net>
2012-03-15 18:56:35 +0100

    this is way easier - use shipping_instructions config setting

--- app/overrides/add_details_field_to_shipping.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "checkout/_delivery",
-                     :name => "allow specifying details for shipping",
-                     :insert_after => "fieldset",
-                     :text => '<textarea rows="5" cols="50" name="order[shipping_details]" id="order_shipping_details">Bitte geben sie eventuelle Urlaubszeiten oder andere Anmerkungen zum Versand an.</textarea>')
