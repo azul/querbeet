6d3e36db8f8dcf9492ef2544690c3f87e3dd4f5b
Azul <azul@riseup.net>
2012-03-16 08:35:20 +0100

    some copy for the terms of service checkbox

--- app/overrides/add_terms_of_service_checkbox.rb
+++ app/overrides/add_terms_of_service_checkbox.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "checkout/_payment",
                      :name => "insert terms of service checkbox",
                      :insert_after => '#payment',
-                     :text => '<%= check_box(:order, :terms_of_service) %>')
+                     :partial => 'checkout/terms_of_service')
