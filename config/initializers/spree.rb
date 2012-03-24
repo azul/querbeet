1c4ce5adf33be76280805ed8bf911ec767fc0e50
Azul <azul@riseup.net>
2012-03-24 11:35:45 +0100

    we now have more than 100 products - let's still display all of them.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,7 +1,7 @@
 Spree::Config.set(:allow_ssl_in_production => false)
 Spree::Config.set(:logo => '/assets/store/querbeet_header.jpg')
 Spree::Config.set(:address_requires_state => false)
-Spree::Config.set(:products_per_page => 100)
+Spree::Config.set(:products_per_page => 200)
 Spree::Config.set(:checkout_zone => 'Deutschland')
 Spree::Config.set(:html_invoice_logo_path => "store/querbeet_header.jpg")
 Spree::Config.set(:shipping_instructions => true)
