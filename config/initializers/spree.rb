51a7046c6fd34a952580c8572de33e383389dc24
Azul <azul@riseup.net>
2012-06-16 11:25:38 +0200

    make sure we have a Spree::Config instance before trying to set preferences

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,7 +1,9 @@
-Spree::Config.set(:allow_ssl_in_production => false)
-Spree::Config.set(:logo => '/assets/store/querbeet_header.jpg')
-Spree::Config.set(:address_requires_state => false)
-Spree::Config.set(:products_per_page => 200)
-Spree::Config.set(:checkout_zone => 'Deutschland')
-Spree::Config.set(:html_invoice_logo_path => "store/querbeet_header.jpg")
-Spree::Config.set(:shipping_instructions => true)
+if Spree::Config.instance
+  Spree::Config.set(:allow_ssl_in_production => false)
+  Spree::Config.set(:logo => '/assets/store/querbeet_header.jpg')
+  Spree::Config.set(:address_requires_state => false)
+  Spree::Config.set(:products_per_page => 200)
+  Spree::Config.set(:checkout_zone => 'Deutschland')
+  Spree::Config.set(:html_invoice_logo_path => "store/querbeet_header.jpg")
+  Spree::Config.set(:shipping_instructions => true)
+end
